e3d6b1425835593a29a849dbc64b1aca30d66209
Arkitektz <info@arkitektz.com>
2021-12-10 02:18:43 +0500

    working function

--- BucketDeleteApp/BucketDelete.cs
+++ BucketDeleteApp/BucketDelete.cs
@@ -1,13 +1,13 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Data;
 using System.Data.SqlClient;
-using System.Threading.Tasks;
-using System.Data.Common;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using System.Net.Http;
 
 namespace BucketDeleteApp
 {
@@ -24,7 +24,7 @@ namespace BucketDeleteApp
             {
                 log.LogInformation($"BucketDelete function database successfully connected at : {DateTime.Now}");
                 //conn.Open();
-                var text = @"select dr.UnitId, st.DigitalFile from DeleteRequest dr 
+                var text = @"select dr.UnitId, st.DigitalFile, st.username from DeleteRequest dr 
                             inner join StorageUnit st on dr.UnitId = st.UnitId 
                             where dr.Process = 0";
 
@@ -42,12 +42,13 @@ namespace BucketDeleteApp
                     {
                         var unitId = row["UnitId"].ToString();
                         var digitalFile = row["DigitalFile"].ToString();
+                        var username = row["username"].ToString();
 
                         log.LogInformation($"Deleting file : " + digitalFile + " - unitid : " + unitId );
 
                         var response = DeleteFile(digitalFile);
 
-                        log.LogInformation($"file : " + digitalFile + " - unitid : " + unitId + " deleted successfully, response was : " + response.Status.ToString());
+                        log.LogInformation($"file : " + digitalFile + " - unitid : " + unitId + " deleted successfully, response was : " + response);
 
                         string updateQuery = "Update DeleteRequest Set Process = 1 where UnitId = @unitId;";
                         string deleteQuery = "Delete from StorageUnit where UnitId = @unitId;";
@@ -61,6 +62,8 @@ namespace BucketDeleteApp
                         updateCommand.ExecuteNonQuery();
                         conn.Close();
 
+                        CallPostApi(digitalFile, username);
+
                         log.LogInformation($"BucketDelete function database successfully updated after deleting the file.");
                     }
                 }
@@ -68,20 +71,56 @@ namespace BucketDeleteApp
             }
         }
 
-
-        public static async Task<DeleteObjectResponse> DeleteFile(String filename)
+        private static HttpResponseMessage CallPostApi(string digitalFile, string username)
         {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("ApiUrl"));
+
+                //creating object
+                PostDeleteRequest obj = new PostDeleteRequest()
+                {
+                    ClientCode = "AAA",
+                    Program = "DELETE",
+                    Description1 = "N",
+                    Description2 = digitalFile,
+                    Username = username
+                };
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync("web/common/DigitalFile", obj);
+                postTask.Wait();
+
+                return postTask.Result;                
+            }
+        }
+
+        public static DeleteObjectResponse DeleteFile(String filename)
+        {
+            string connectionStringKeyVault = string.Format("RunAs=App;AppId={0};{1}{2};{3}{4}", Environment.GetEnvironmentVariable("ClientID"), "TenantId=", Environment.GetEnvironmentVariable("TenantID"), "AppKey=", Environment.GetEnvironmentVariable("ClientSecret"));
+            var azureServiceTokenProvider = new AzureServiceTokenProvider(connectionStringKeyVault);
+            string s3SecretKey, s3Id;
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback.Invoke));
+            s3Id = keyVaultClient.GetSecretAsync(string.Format("{0}{1}", Environment.GetEnvironmentVariable("keyVaultURI"), "S3ID")).Result.Value;
+            s3SecretKey = keyVaultClient.GetSecretAsync(string.Format("{0}{1}", Environment.GetEnvironmentVariable("keyVaultURI"), "S3Secret")).Result.Value;
+            var s3Url = keyVaultClient.GetSecretAsync(string.Format("{0}{1}", Environment.GetEnvironmentVariable("keyVaultURI"), "S3URL")).Result.Value;
+
+            var config = new AmazonS3Config();
+            config.ServiceURL = s3Url; 
+            config.ForcePathStyle = true;
+            var s3Client = new AmazonS3Client(s3Id, s3SecretKey, config);
+
             String key = filename;
-            string _foldername = "images";
-            string _keyPublic = Environment.GetEnvironmentVariable("aws_accesskeyid");
-            string _keySecret = Environment.GetEnvironmentVariable("aws_secretkey");
+            string _foldername = Environment.GetEnvironmentVariable("aws_foldername"); //"images";
+            string _keyPublic = Environment.GetEnvironmentVariable("s3Id");
+            string _keySecret = Environment.GetEnvironmentVariable("s3SecretKey");
             string _bucket = Environment.GetEnvironmentVariable("aws_bucket");
-            var amazonClient = new AmazonS3Client(_keyPublic, _keySecret, Amazon.RegionEndpoint.APSouth1);
 
             var deleteObjectRequest = new DeleteObjectRequest { BucketName = _bucket, Key = String.Format("{0}/{1}", _foldername, key) };
-            var response = amazonClient.DeleteObjectAsync(deleteObjectRequest).GetAwaiter().GetResult();
+            var response = s3Client.DeleteObjectAsync(deleteObjectRequest).GetAwaiter().GetResult();
 
             return response;
         }
+
     }
 }
---
+++ BucketDeleteApp/PostDeleteRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BucketDeleteApp
+{
+    internal class PostDeleteRequest
+    {
+        public string ClientCode { get; set; }
+        public string Program { get; set; }
+        public string Description1 { get; set; }
+        public string Description2 { get; set; }
+        public string Username { get; set; }
+    }
+}
